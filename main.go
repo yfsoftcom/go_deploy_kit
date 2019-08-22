f1b96a91f3bee5907c62d5ea4020b0620654934b
wangfan <wangfan@pc2>
2019-08-23 07:59:25 +0800

    add a ui for deploy

--- main.go
+++ main.go
@@ -46,7 +46,69 @@ func RunScriptFile(command DeployCommand) (string, error){
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "{ \"errno\": 0}" )
+	
+	fmt.Fprintf(w, `<!DOCTYPE html>
+<html>
+	<head>
+		<title>GDK beta</title>	
+		<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+		<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+		<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
+		<link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+	<body class="bg-gray">
+	<div class="container">
+	<div class="hero bg-gray">
+		<div class="hero-body">
+			<div class="login">
+				<div class="empty">
+					<div class="empty-icon"><i class="icon icon-3x icon-emoji"></i></div>
+					<p class="empty-title h5">Welcome To Use Go-Deploy-Kit!</p>
+					<p class="empty-subtitle">Start to enjoy the kit!</p>
+					<div class="column">
+						<form  enctype="multipart/form-data" 
+							style="margin-left: auto; margin-right: auto; max-width: 600px;"  
+							class="form-horizontal" 
+							action="/upload" 
+							method="POST" >
+							<div class="form-group">
+								<div class="col-3">
+									<label class="form-label">Jar:</label>
+								</div>
+								<div class="col-9">
+									<input class="form-input" name="file" type="file">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-3">
+									<label class="form-label">Shell:</label>
+								</div>
+								<div class="col-9">
+									<input class="form-input" name="shell" type="text">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-3">
+									<label class="form-label">Argument:</label>
+								</div>
+								<div class="col-9">
+									<textarea class="form-input" name="argument"></textarea>
+								</div>
+							</div>
+							<button class="btn btn-success" type="submit"><i class="icon icon-upload"></i> Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	</div>
+	</body>
+</html>
+		`)
+	
 }
 
 // 接受webhook的请求处理函数
@@ -71,6 +133,9 @@ func WebhookHandler(w http.ResponseWriter, r *http.Request) {
 // 上传文件的函数
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	// 
+	if method := r.Method; method == "GET" {
+		fmt.Fprintf(w, "{ \"errno\": -1, \"msg\": \" Error: Only support Post\"}")
+	}
     r.ParseMultipartForm(32 << 20)
 	file, handler, err := r.FormFile("file")
 	if err != nil {
@@ -86,6 +151,22 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer f.Close()
 	io.Copy(f, file)
+
+	if shell := r.PostFormValue("shell"); shell != ""{
+		argument := r.PostFormValue("argument")
+		// 执行脚本
+		command := & DeployCommand {
+			Script: shell,
+			Argument: argument,
+		}
+		if output, err := RunScriptFile( *command); err != nil {
+			fmt.Fprintf(w, "{ \"errno\": -1, \"msg\": \" Error:" + err.Error() +"\"}")
+		}else {
+			fmt.Fprintf(w, "{ \"errno\":0 , \"msg\":\"" + output + "\"}" )
+			return;
+		}
+	}
+
 	fmt.Fprintf(w, "{ \"errno\":0 , \"msg\":\"upload success\"}" )
 }
 
